c7bcb6f539ab20adb854389f4955d83f279c276f
Jaden Grandgarden <danganh2000@gmail.com>
2025-02-26 18:21:37 +0000

    P97/P95/P90 Taxi Monthly Fare

--- 04-analytics-engineering/taxi_rides_ny/models/core/fct_taxi_trips_quarterly_revenue.sql
+++ 04-analytics-engineering/taxi_rides_ny/models/core/fct_taxi_trips_quarterly_revenue.sql
@@ -3,7 +3,6 @@
 with trips_data as (
     select * from {{ ref('fact_trips') }}
 )
--- , with fct_taxi_trips_year_quarterly_revenue as (
 select service_type,
 year_quarter,
 quarter,
@@ -13,12 +12,3 @@ from trips_data
 where pickup_year = 2019 OR pickup_year=2020
 group by 1,2,3,4
 order by 1,3,4
--- )
--- SELECT service_type,
---     year_quarter,
---     quarter,
---     pickup_year, 
---     round((revenue_year_quarter_total_amount - lag(revenue_year_quarter_total_amount) * 100/lag(revenue_year_quarter_total_amount)
---              OVER ( PARTITION BY service_type, quarter order by pickup_year ), 2) as percentage_change  
--- FROM fct_taxi_trips_year_quarterly_revenue 
--- order by 1,3,4
